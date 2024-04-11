f1022e99cc27a4bc6b16115c31263a263c8324f5
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-04-11 20:41:34 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,10 @@
 def slice_advanced():
+    def slice_advanced():
+hello=input("say hello to the world!: ")
+print(hello[4::2])
+
+count=imput(12345678910)
+print(count[4::2])
     # Código a implementar utilizando input.
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
