f5ecd3969803df5b44b789631bc30a9655e7b33f
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-04-11 15:25:40 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,27 @@
 def check_vowels():
     # Código a implementar utilizando input.
-
+    name=imput("ingrese su nombre: ")
+    a=print("a" in name)
+    e=print("e" in name)
+    i=print("i" in name)
+    o=print("o" in name)
+    u=print("u" in name)  
+    print(f"contiene:{a}")
+    print(f"contiene:{e}")
+    print(f"contiene:{i}")
+    print(f"contiene:{o}")
+    print(f"contiene:{u}")
+    name2=imput("ingrese su nombre: ")
+    a=print("a" in name2)
+    e=print("e" in name2)
+    i=print("i" in name2)
+    o=print("o" in name2)
+    u=print("u" in name2)  
+    print(f"contiene:{a}")
+    print(f"contiene:{e}")
+    print(f"contiene:{i}")
+    print(f"contiene:{o}")
+    print(f"contiene:{u}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
